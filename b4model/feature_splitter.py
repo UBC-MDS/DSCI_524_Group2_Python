aec9e844530174c615bb71142a810b79aaed96f6
Sakiga <54461529+eyrakas@users.noreply.github.com>
2020-02-26 23:41:20 -0800

    feature splitter python file added

---
+++ b4model/feature_splitter.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def feature_splitter(x):
+    """ Splits dataset column names into a tupple of categorical and numerical lists
+
+    Parameters
+    ----------
+    x : DateFrame
+
+    Returns
+    -------
+    tupple: 
+        tupple of two lists
+    
+    Example
+    -------
+    >>> feature_splitter(data)
+    ([categorical:],[numerical: ])
+    """
+    #code goes here
+
+    pass
